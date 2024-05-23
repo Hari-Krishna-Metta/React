ea4e25ee64ae97d8eb42c13238b136e354e4686c
Hari-Krishna-Metta <135844434+Hari-Krishna-Metta@users.noreply.github.com>
2024-05-23 18:54:55 +0530

    Add files via upload

---
+++ Episode 02/Code/App.js
@@ -0,0 +1,28 @@
+// <div id = "parent"> 
+//     <div id = "child1"> 
+//     <h1>I'm a h1 tag</h1>
+//     <h2>I'm a h2 tag</h2>
+//     </div>
+//     <div id = "child2"> 
+//         <h1>I'm a h1 tag</h1>
+//         <h2>I'm a h2 tag</h2>
+//     </div>
+// </div>
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag")
+    ]), 
+    React.createElement("div", {id: "child1"}, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag")
+    ])
+]);
+
+
+const parent = ReactDOM.createRoot(document.getElementById("root"));
+parent.render(heading);
