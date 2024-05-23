9d8579b6a4560ad540d2bd29877c2a6aaeb8d15e
Hari-Krishna-Metta <135844434+Hari-Krishna-Metta@users.noreply.github.com>
2024-05-23 10:46:37 +0530

    Update App.js

--- Episode 01/Code/App.js
+++ Episode 01/Code/App.js
@@ -1,9 +1,21 @@
+// <div id = "parent"> 
+//     <div id = "child1"> 
+//     <h1>I'm a h1 tag</h1>
+//     <h2>I'm a h2 tag</h2>
+//     </div>
+//     <div id = "child2"> 
+//         <h1>I'm a h1 tag</h1>
+//         <h2>I'm a h2 tag</h2>
+//     </div>
+// </div>
+
+
 const heading = React.createElement("div", {id: "parent"}, [
     React.createElement("div", {id: "child1"}, [
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I'm an h2 tag")
     ]), 
-    React.createElement("div", {id: "child1"}, [
+    React.createElement("div", {id: "child2"}, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag")
     ])
